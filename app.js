daca0b56c0421403409b8f690e18e83bb3bb90c6
SAMANTHA PARKES <slmillward@outlook.com>
2025-09-18 15:30:38 +0100

    api and click buttons

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ let state = {
   cps: 1,
 };
 
+const cpsDisplay = document.getElementById("cps");
+const cookieDisplay = document.getElementById("cookies");
+const shopDisplay = document.getElementById("shop");
+
 // increase cookieCount by cookieCount+cps
 setInterval(function () {
   state.cookieCount = state.cookieCount + state.cps;
   //display count on page
-  document.getElementById("cookies").innerText = state.cookieCount;
+  cookieDisplay.innerText = "Cookie count : " + state.cookieCount;
   //save state to local storage using setItem
   saveStateToLocal();
 }, 1000);
@@ -31,15 +35,50 @@ state.cps = returnedState.cps;
 const cookieImage = document.getElementById("cookieImage");
 cookieImage.addEventListener("click", function () {
   state.cookieCount++;
-  document.getElementById("cookies").innerText = state.cookieCount;
+  document.getElementById("cookies").innerText =
+    "Cookie count : " + state.cookieCount;
 });
 
 //show cps count in p tag
-const cpsDisplay = document.getElementById("cps");
 cpsDisplay.innerText = "CPS: " + state.cps;
 
-//get shop items displayed on screen from api
-//async function with await fetch and .json()
+//get upgrade data from api using async function with await fetch and .json()
+async function getUpgrades() {
+  const response = await fetch(
+    "https://cookie-upgrade-api.vercel.app/api/upgrades"
+  );
+  const data = await response.json();
+  return data;
+}
+
+//get upgrade data from api to show on webpage as buy items using forEach, create elements and append
+async function displayUpgrades() {
+  const upgrades = await getUpgrades();
+  upgrades.forEach(function (shopUpgrade) {
+    //create button
+    const upgradeButton = document.createElement("button");
+    //add text
+    upgradeButton.innerText = `${shopUpgrade.name} (Cost: ${shopUpgrade.cost}, Increase: ${shopUpgrade.increase})`;
+    //add click event
+    upgradeButton.addEventListener("click", function () {
+      //if cookie count is more than cost, add the increase of the upgrade to cps and minus cost of the upgrade from cookie count
+      //otherwise alert the user they don't have enough cookies
+      if (state.cookieCount >= shopUpgrade.cost) {
+        state.cps = state.cps + shopUpgrade.increase;
+        state.cookieCount = state.cookieCount - shopUpgrade.cost;
+        cpsDisplay.innerText = "CPS: " + state.cps;
+        cookieDisplay.innerText = "Cookie count: " + state.cookieCount;
+      } else {
+        alert("You don't have enough cookies!");
+      }
+    });
+
+    //append
+    shopDisplay.appendChild(upgradeButton);
+  });
+}
+
+displayUpgrades();
 
 //the upgrades are an array of objects
 
